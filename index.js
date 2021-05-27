fdf46ae399b3b24da152a1090c5c5ed4b941fb26
Desmond Tuiyot <dtuiyot@gmail.com>
2021-05-26 23:56:38 -0400

    removed hardcoded mongodb url

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use("/words", wordRoutes);
 
 const CONNECTION_URL = process.env.MONGODB_URI;
-// "mongodb+srv://dtuiyot:dtuiyot1234@cluster0.6zkxx.mongodb.net/ReverseIPA?retryWrites=true&w=majority";
+
 const PORT = process.env.PORT || 5000;
 
 mongoose
