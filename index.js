7aab376874c807506c429cd6ec5b4361d990a52b
Desmond Tuiyot <dtuiyot@gmail.com>
2021-05-26 23:39:22 -0400

    added dotenv and refactored mongodb uri to environment variable

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 import wordRoutes from "./routes/words.js";
+
+dotenv.config();
 
 const app = express();
 
@@ -12,8 +15,8 @@ app.use(cors());
 
 app.use("/words", wordRoutes);
 
-const CONNECTION_URL =
-  "mongodb+srv://dtuiyot:dtuiyot1234@cluster0.6zkxx.mongodb.net/ReverseIPA?retryWrites=true&w=majority";
+const CONNECTION_URL = process.env.MONGODB_URI;
+// "mongodb+srv://dtuiyot:dtuiyot1234@cluster0.6zkxx.mongodb.net/ReverseIPA?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 mongoose
